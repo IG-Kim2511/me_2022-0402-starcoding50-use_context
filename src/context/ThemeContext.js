1a2cf24ec848ed5e108bf5985b52b09d10a0c18e
IG <iikim2511@gmail.com>
2023-09-11 21:41:30 -0400

    ---

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,13 +3,12 @@
 
 
 /* 🦄2023-0911
-    0.ㄱ context만듬
+    0.ㄱ context만듬 : createContext
 
-    1. <ㄱcontext.Provder value={{ㄴ,ㄷ}}> 있는곳에 변수 넣음
+    1. <ㄱcontext.Provder value={{ㄴ,ㄷ}}> 있는곳에 변수 넣음 : Provder , 변수
     👉App.js
 
-    2. 사용하는곳에서 
-    const {ㄴ,ㄷ} = useContext(ㄱcontext) 넣고 변수 사용
+    2. 사용하는곳에서 const {ㄴ,ㄷ} = useContext(ㄱcontext) 넣고 변수 사용 : useContext
     👉Footer.js
 */
 import { createContext } from "react";  
